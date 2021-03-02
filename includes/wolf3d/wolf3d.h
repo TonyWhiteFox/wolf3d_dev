2bc8de4996607a7e8bcd4ed90b0683052466719d
Lymond mallister Donnor stark <ldonnor-@oa-g3.msk.21-school.ru>
2021-03-02 21:56:15 +0300

    02_03 инициализация окна, стартовые параметры для игрока

--- includes/wolf3d/wolf3d.h
+++ includes/wolf3d/wolf3d.h
@@ -10,10 +10,46 @@
 #  include <CL/cl.h>
 # endif
 
+typedef struct			s_mlx
+{
+	void				*mlx_ptr;
+	void				*win_ptr;
+	void				*img_ptr;
+	void				*img_adr;
+	int					bpp;
+	int					stride;
+	int					endian;
+	bool				stop;
+}						t_mlx;
+
+typedef struct			s_player
+{
+	int					position_x;
+	int					position_y;
+	int					position_z;
+	int					stay_size;
+	int					sit_size;
+	int					widht;
+	int					standart_speed;
+	int					sit_speed;
+	int					run_speed;
+	int					angle_around;
+	int					angle_horizon;
+	int					angle_horizon_low_limit;
+	int					angle_horizon_hight_limit;
+}						t_player;
+
+
 typedef struct			s_wolf
 {
 	int					*cl;
-
+	char				*map_name;
+	t_mlx				*mlx;
+	t_player			*player;
+	int					win_size_x;
+	int					win_size_y;
 }						t_wolf;
+
+void					get_error_with_text(char *message, bool memory_alowed);
 
 #endif
